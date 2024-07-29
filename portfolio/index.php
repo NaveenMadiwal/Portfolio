3ddcfdef55df4a1fe23c4a9659b97f2f9c05fc71
NaveenMadiwal <naveenmadiwal7777@gmail.com>
2024-07-29 07:20:53 +0530

    home page

---
+++ portfolio/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+  <nav class="navbar">
+    <div class="navbar-container">
+      <div class="logo">
+        <a href="#" class="navbar-logo">Naveen Madiwal</a>
+      </div>
+      <div class="menus">
+        <ul class="navbar-menu">
+          <li class="navbar-item"><a href="#home" class="navbar-link">Home</a></li>
+          <li class="navbar-item"><a href="#about" class="navbar-link">Skills</a></li>
+          <li class="navbar-item"><a href="#services" class="navbar-link">Projects</a></li>
+          <li class="navbar-item"><a href="#contact" class="navbar-link">About</a></li>
+          <li class="navbar-item"><a href="#contact" class="navbar-link">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <video autoplay loop muted playsinline id="background-video">
+    <source src="medias/background-video.mp4" type="video/mp4">
+  </video>
+
+  <section id="main-sec" class="clearfix">
+
+  <div id="aboutMe">
+  <div class="heading">
+    <h1>NAVEEN MADIWAL</h1>
+  </div>
+  <div class="des">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam repellat totam quo! Distinctio consequatur quibusdam soluta impedit dignissimos eaque fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Aliquam repellat totam quo! Distinctio consequatur quibusdam soluta impedit dignissimos eaque fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam repellat totam quo! Distinctio consequatur quibusdam soluta impedit dignissimos eaque fugiat!</p>
+  </div>
+
+  <div class="icons">
+    <a href="#" class="git" ><i class="fab fa-github" style="color:white"></i></a>
+    <a href="#" class="linkedin"><i class="fab fa-linkedin" style="color:#2868b2"></i></a>
+    <a href="#" class="gmail"><i class="fas fa-envelope" style="color:#ec4134"></i></a>
+  </div>
+  <div class="resume-btn">
+    <button> <a href="./medias/Naveen_Software_Developer.pdf" class="r-btn" target="_blank">Download Resume</a> </button>
+  </div>
+</div>
+
+    <div id="thumbnail">
+      <img src="./medias/banner.webp" alt="">
+    </div>
+  </section>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
